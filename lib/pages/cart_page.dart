bf78ae1145fb39430d08fde280e8210e8c6cc1cc
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-05-19 08:13:24 -0600

    commit 1 19May2025

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,37 @@
+import 'package:e401_ecommerce/models/tienda.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    final carrito = context.watch<Tienda>().carrito;
+
     return Scaffold(
       appBar: AppBar(
+        foregroundColor: theme.inversePrimary,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         title: Text('Carrito de Compras'),
+      ),
+      backgroundColor: theme.surface,
+      body: Column(
+        children: [
+          ListView.builder(
+            itemCount: carrito.length,
+            itemBuilder: (context, index) {
+              final item = carrito[index];
+
+              return ListTile(
+                
+              );
+            },
+          ),
+        ],
       ),
     );
   }
